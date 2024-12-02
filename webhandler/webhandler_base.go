7300614e36c3ee03483946f8ae62565f3a475839
Yoshi-Hirai <marinos.yoshi@gmail.com>
2024-12-02 18:11:55 +0900

    リファクタリング(命名規則の対応など)

--- webhandler/webhandler_base.go
+++ webhandler/webhandler_base.go
@@ -1,5 +1,5 @@
-// pkg_webhandler WebHandlerパッケージ
-package pkg_webhandler // パッケージ名はディレクトリ名と同じにする
+// webhandler webhandlerパッケージ
+package webhandler // パッケージ名はディレクトリ名と同じにする
 
 import (
 	"log/slog"
@@ -9,8 +9,8 @@ import (
 // ---- Package Global Variable
 
 // ---- public function ----
-// Webhandler_init (public)Webhandlerシステムの初期化関数。handlerを登録し指定されたポートでサーバーを立ち上げる
-func Webhandler_init(port string) error {
+// WebHandlerInit (public)Webhandlerシステムの初期化関数。handlerを登録し指定されたポートでサーバーを立ち上げる
+func WebHandlerInit(port string) error {
 	slog.Info("WebHandler Open", "port", port)
 	webhandlerErr := http.ListenAndServe(":"+port, nil)
 	if webhandlerErr != nil {
